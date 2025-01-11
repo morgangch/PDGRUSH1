25f7c31a709500ed9cbdd6a0bf476a99f7d946ed
lilyan-bouyer <lilyan.bouyer@epitech.eu>
2025-01-11 10:44:19 +0100

    fix part of coding style and add fonctions to rotate etc.

--- Main.hs
+++ Main.hs
@@ -1,3 +1,10 @@
+{-
+-- EPITECH PROJECT, 2025
+-- rush1
+-- File description:
+-- Main
+-}
+
 module Main where
 import System.Exit()
 
@@ -21,6 +28,36 @@ parseArgs [_] = putStrLn "Incomplete argument"
 parseArgs (x:y:xs)
     | isOperator [x,y] = putStrLn [x,y] >> parseArgs xs
     | otherwise = parseArgs xs
+
+swap :: [a] -> [a]
+swap [] = []
+swap [x] = [x]
+swap (x:y:xs) = y : x : xs
+
+swapb :: [a] -> [a] -> ([a], [a])
+swapb l1 l2 = (swap l1, swap l2)
+
+pa :: [a] -> [a] -> ([a], [a])
+pa l1 [] = (l1, [])
+pa l1 (x:xs) = (x : l1, xs)
+
+pb :: [a] -> [a] -> ([a], [a])
+pb [] l2 = ([], l2)
+pb (x:xs) l2 = (xs, x : l2)
+
+rotate :: [a] -> [a] 
+rotate [] = []
+rotate (x:xs) = xs ++ [x]
+
+rr :: [a] -> [a] -> ([a], [a])
+rr l1 l2 = (rotate l1, rotate l2)
+
+rotaterev :: [a] -> [a]
+rotaterev [] = []
+rotaterev list = last list : init list
+
+rrr :: [a] -> [a] -> ([a], [a])
+rrr l1 l2 = (rotaterev l1, rotaterev l2)
 
 main :: IO ()
 main = do
