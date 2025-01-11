e77cfb28fc5300e514c0f2f85d7400b6110b1c10
lilyan-bouyer <lilyan.bouyer@epitech.eu>
2025-01-11 11:38:52 +0100

    fix / modif command checker.

--- CommandChecker.hs
+++ CommandChecker.hs
@@ -14,30 +14,15 @@ isSorted :: [Int] -> Bool
 isSorted xs = xs == sort xs
 
 doOperation :: ([Int], [Int]) -> String -> ([Int], [Int])
-doOperation (l_a, l_b) op = 
-    let (new_l_a, new_l_b) = doOperationA (l_a, l_b) op
-        (final_l_a, final_l_b) = doOperationB (new_l_a, new_l_b) op
-    in doOperationC (final_l_a, final_l_b) op
-
-doOperationA :: ([Int], [Int]) -> String -> ([Int], [Int])
-doOperationA (l_a, l_b) op = case op of
-    "sa" -> (swap l_a, l_b)
-    "ra" -> (rotate l_a, l_b)
-    "rra" -> (rotaterev l_a, l_b)
-    _    -> (l_a, l_b)
-
-doOperationB :: ([Int], [Int]) -> String -> ([Int], [Int])
-doOperationB (l_a, l_b) op = case op of
-    "sb" -> (l_a, swap l_b)
-    "rb" -> (l_a, rotate l_b)
-    "rrb" -> (l_a, rotaterev l_b)
-    _    -> (l_a, l_b)
-
-doOperationC :: ([Int], [Int]) -> String -> ([Int], [Int])
-doOperationC (l_a, l_b) op = case op of
-    "sc" -> swapb (l_a, l_b)
-    "pa" -> pa (l_a, l_b)
-    "pb" -> pb (l_a, l_b)
-    "rr" -> rr (l_a, l_b)
-    "rrr" -> rrr (l_a, l_b)
-    _    -> (l_a, l_b)
+doOperation (l_a, l_b) "sa" = (swap l_a, l_b)
+doOperation (l_a, l_b) "sb" = (l_a, swap l_b)
+doOperation (l_a, l_b) "sc" = swapb l_a l_b
+doOperation (l_a, l_b) "pa" = pa l_a l_b
+doOperation (l_a, l_b) "pb" = pb l_a l_b
+doOperation (l_a, l_b) "ra" = (rotate l_a, l_b)
+doOperation (l_a, l_b) "rb" = (l_a, rotate l_b)
+doOperation (l_a, l_b) "rr" = rr l_a l_b
+doOperation (l_a, l_b) "rra" = (rotaterev l_a, l_b)
+doOperation (l_a, l_b) "rrb" = (l_a, rotaterev l_b)
+doOperation (l_a, l_b) "rrr" = rrr l_a l_b
+doOperation (l_a, l_b) _ = (l_a, l_b)
