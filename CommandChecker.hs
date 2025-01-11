23c00cef9d9f7085e1da26c699d05ea1617cdf17
Noa González <noa.gonzalez-gomez@epitech.eu>
2025-01-11 11:12:35 +0100

    [ADD] Sorted doOperations to be more precise in what list has to change.

--- CommandChecker.hs
+++ CommandChecker.hs
@@ -14,7 +14,8 @@ isSorted xs = xs == sort xs
 doOperation :: ([Int], [Int]) -> String -> ([Int], [Int])
 doOperation (l_a, l_b) op = 
     let (new_l_a, new_l_b) = doOperationA (l_a, l_b) op
-    in doOperationB (new_l_a, new_l_b) op
+        (final_l_a, final_l_b) = doOperationB (new_l_a, new_l_b) op
+    in doOperationC (final_l_a, final_l_b) op
 
 doOperationA :: ([Int], [Int]) -> String -> ([Int], [Int])
 doOperationA (l_a, l_b) op = case op of
@@ -28,6 +29,10 @@ doOperationB (l_a, l_b) op = case op of
     "sb" -> (l_a, sb l_b)
     "rb" -> (l_a, rb l_b)
     "rrb" -> (l_a, rrb l_b)
+    _    -> (l_a, l_b)
+
+doOperationC :: ([Int], [Int]) -> String -> ([Int], [Int])
+doOperationC (l_a, l_b) op = case op of
     "ss" -> ss (l_a, l_b)
     "pa" -> pa (l_a, l_b)
     "pb" -> pb (l_a, l_b)
