401dd58c611581b318129603950a136db67afed2
Noa González <noa.gonzalez-gomez@epitech.eu>
2025-01-11 11:16:31 +0100

    [FIX] Changed the named of the functions called in CommandChecker.hs.

--- CommandChecker.hs
+++ CommandChecker.hs
@@ -19,21 +19,21 @@ doOperation (l_a, l_b) op =
 
 doOperationA :: ([Int], [Int]) -> String -> ([Int], [Int])
 doOperationA (l_a, l_b) op = case op of
-    "sa" -> (sa l_a, l_b)
-    "ra" -> (ra l_a, l_b)
-    "rra" -> (rra l_a, l_b)
+    "sa" -> (swap l_a, l_b)
+    "ra" -> (rotate l_a, l_b)
+    "rra" -> (rotaterev l_a, l_b)
     _    -> (l_a, l_b)
 
 doOperationB :: ([Int], [Int]) -> String -> ([Int], [Int])
 doOperationB (l_a, l_b) op = case op of
-    "sb" -> (l_a, sb l_b)
-    "rb" -> (l_a, rb l_b)
-    "rrb" -> (l_a, rrb l_b)
+    "sb" -> (l_a, swap l_b)
+    "rb" -> (l_a, rotate l_b)
+    "rrb" -> (l_a, rotaterev l_b)
     _    -> (l_a, l_b)
 
 doOperationC :: ([Int], [Int]) -> String -> ([Int], [Int])
 doOperationC (l_a, l_b) op = case op of
-    "ss" -> ss (l_a, l_b)
+    "sc" -> swapb (l_a, l_b)
     "pa" -> pa (l_a, l_b)
     "pb" -> pb (l_a, l_b)
     "rr" -> rr (l_a, l_b)
